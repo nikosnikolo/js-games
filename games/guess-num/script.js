59ecb7d05dc0ce13d93c347f0098d4db8e0a33b3
nikosnikolo <nnikolopoulos761@iekakmi.edu.gr>
2026-06-01 13:31:21 +0300

    diortgosh No5 teliko

--- games/guess-num/script.js
+++ games/guess-num/script.js
@@ -2,16 +2,16 @@ let score = 0;
 let number = Math.floor(Math.random() * 10) + 1;
 
 function check() {
-
     let guess = Number(document.getElementById("guess").value);
 
     if (guess == number) {
         score++;
         document.getElementById("result").innerText =
-            "swsta to score sou einai: " + score;
-    }
-    else {
+            "swsta ,to score soy einai: " + score;
+
+        number = Math.floor(Math.random() * 10) + 1; // νέο νούμερο
+    } else {
         document.getElementById("result").innerText =
-            "lathos ,dokimase xana";
+            "lathos, dokimase xana";
     }
 }
